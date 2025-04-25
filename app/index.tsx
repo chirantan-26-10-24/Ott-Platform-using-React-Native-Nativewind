cb4644ba0dc13a22bdb580cd68b4e122b2c9769d
chirantan-26-10-24 <iweb.dev.mi01@gmail.com>
2025-04-26 01:25:15 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-5xl text-dark-200 font-bold">Welcome !</Text>
+			<Link href="/onboarding">Onboarding</Link>
+			<Link
+				href={{
+					pathname: "/movie/[id]",
+					params: { id: "avengers" },
+				}} 
+			>
+				Avengers Movie
+			</Link>
+		</View>
+	);
+}
