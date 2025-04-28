31f347d314ca267e6b7f3ead3f4fef802c556595
chirantan-26-10-24 <iweb.dev.mi01@gmail.com>
2025-04-28 18:55:12 +0530

    api calling done

---
+++ services/useFetch.ts
@@ -0,0 +1,36 @@
+//fetchMovies
+//fetchMovieDetails
+
+import { useEffect, useState } from "react";
+
+//useFetch(fetchMovies)
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+	const [data, setData] = useState<T | null>(null);
+	const [error, setError] = useState<string | null>(null);
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const fetchData = async () => {
+		try {
+			setLoading(true);
+			setError(null);
+			const result = await fetchFunction();
+			setData(result);
+		} catch (err) {
+			//@ts-ignore
+			setError(err instanceof Error ? err : new Error("An error occurred"));
+		} finally {
+			setLoading(false);
+		}
+	};
+	const reset = () => {
+		setData(null);
+		setError(null);
+		setLoading(false);
+	};
+	useEffect(() => {
+		if (autoFetch) {
+			fetchData();
+		}
+	}, []);
+	return { data, error, loading, refetch: fetchData, reset };
+};
